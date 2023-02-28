a9a354d4cd7c5087edc38c75cd90e47aeb16c3fd
Samee Rauf <sameerauf1@gmail.com>
2023-02-28 14:39:42 -0800

    parsing lines

--- algorithm1.py
+++ algorithm1.py
@@ -3,9 +3,16 @@ def Cities_in_California():
         # all the lines in the text file
         file_lines = file.readlines()
     # loop through each line in the text file
+    counter = 0
     for line in file_lines:
+        counter += 1
         arrays = line.split(' = ')
+        if counter == 3:
+            print(arrays[0])
+            print(arrays[1])
         print(arrays)
+        if counter == 3:
+            break
 
         # print(line)
 print("--------------------------------------------------------------------------------------------------------------------------------")
