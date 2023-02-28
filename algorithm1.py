f983fb309f525715556ff09254ad5e588cdfa88e
Samee Rauf <sameerauf1@gmail.com>
2023-02-28 13:27:51 -0800

    can read file

--- algorithm1.py
+++ algorithm1.py
@@ -1,1 +1,13 @@
-print("hi")
+def Cities_in_California():
+    with open('in2a.txt', 'r') as file:
+        # all the lines in the text file
+        file_lines = file.readlines()
+    # loop through each line in the text file
+    for line in file_lines:
+        arrays = line.split(' = ')
+        print(arrays)
+
+        # print(line)
+print("--------------------------------------------------------------------------------------------------------------------------------")
+
+Cities_in_California()
